95165f047b7fc032bd52c313eedb9fa43a5ddd86
zzy19868 <zzy19868@hotmail.com>
2023-02-09 16:30:01 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,8 @@
+cd lede
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/lean/passwall_package
+cd package/lean/passwall_package
+git checkout 4fd4bf8f68fbbbd0d9eca865e68762d6ce3a01de
+cd -
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/lean/passwall
+cp -rf ./package/lean/passwall_package/* ./package/lean/passwall
+rm -rf ./package/lean/passwall_package
